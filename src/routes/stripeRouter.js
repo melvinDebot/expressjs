649a4edc9a1fde918460e6a1970f6db7b925c8fb
Melvin Debot <melvin@exceenis.com>
2023-12-13 14:19:04 +0100

    past mode prod

--- src/routes/stripeRouter.js
+++ src/routes/stripeRouter.js
@@ -4,18 +4,24 @@ import Stripe from "stripe";
 const routes = Router();
 
 // Assurez-vous que votre variable d'environnement STRIPE_SECRET_KEY est définie
-const stripe = new Stripe("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");
+// Prod : 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
+// test : sk_test_51MgTATKww5u0tgAGJjsLFA2mrVDXBlKsal9xaPz8OShrKhOojqj6K8gHufZcDMyViYCodYF8ZqJ0jEFQwyuuzQlM00zloO7hw7
+const stripe = new Stripe(
+  "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"
+);
 
 routes.post("/", async (req, res) => {
   try {
     const session = await stripe.checkout.sessions.create({
       line_items: [
         {
-          price: "price_1OL31AKww5u0tgAGCbuHDHBq", // Remplacez par votre identifiant de prix
+          // prod : price_1OL31AKww5u0tgAGCbuHDHBq
+          // test : price_1OL2uDKww5u0tgAGMJvUrrDp
+          price: "price_1OL2oBKww5u0tgAG9AZTU470", // Remplacez par votre identifiant de prix
           quantity: 1,
         },
       ],
-      
+
       mode: "subscription",
       success_url: `${req.headers.origin}/success`,
       cancel_url: `${req.headers.origin}/cancel`,
